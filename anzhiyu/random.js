1649b2ebcb97c49f5dd24ac8dd486200d1c5f804
zuo xiao <zuoxiao@zuodeMacBook-Pro-2.local>
2023-12-02 20:20:33 +0800

    Site updated: 2023-12-02 20:20:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/08/hello-world/","2023/12/02/LSM-tree/"];function toRandomPost(){
+var posts=["2023/12/02/LSM-tree/","2023/11/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
